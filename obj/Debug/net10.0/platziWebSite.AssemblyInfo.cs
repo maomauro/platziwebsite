a35ccdbce02042e7be46ba39e7f5a3f93501a8a0
maomauro <maomauro.c@gmail.com>
2026-03-04 16:50:08 -0500

    primer ejecucion

--- obj/Debug/net10.0/platziWebSite.AssemblyInfo.cs
+++ obj/Debug/net10.0/platziWebSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("platziWebSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a791b1791ccce0266d3a43acd69cd87d01ea9678")]
 [assembly: System.Reflection.AssemblyProductAttribute("platziWebSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("platziWebSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
